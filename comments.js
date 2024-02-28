bb2838e1739bd8757c3437a8b007d4b0ab47ad0f
Densell Peters III <densell321@gmail.com>
2024-02-28 21:27:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -4,16 +4,16 @@
 // 3. Send the comments.json file to the client
 // 4. Listen on port 3000
 
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  fs.readFile('comments.json', (err, data) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(data);
-  });
-});
+// const server = http.createServer((req, res) => {
+//   fs.readFile('comments.json', (err, data) => {
+//     res.writeHead(200, { 'Content-Type': 'application/json' });
+//     res.end(data);
+//   });
+// });
 
-server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+// server.listen(3000, () => {
+//   console.log('Server is listening on port 3000');
+// });
